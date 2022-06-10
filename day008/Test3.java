1ee7788959d0ae5030a451d0f6447e128cba38a1
yunseohye <97958499+yunseohye@users.noreply.github.com>
2022-06-10 16:27:32 +0900

    Create Test3.java

---
+++ day008/Test3.java
@@ -0,0 +1,16 @@
+package com.day008;
+
+
+public class Test3 {
+
+	public static void main(String[] args) {
+
+//		MyData ob = new MyData();
+//		
+//		ob.print();
+		
+		MyData.getInstance();	
+		
+	}
+
+}
