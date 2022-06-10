d4bce4858019f2fc2a82197dbe77824afff13572
yunseohye <97958499+yunseohye@users.noreply.github.com>
2022-06-10 18:15:50 +0900

    Create ScoreMain2_1.java

---
+++ score2/ScoreMain2_1.java
@@ -0,0 +1,17 @@
+package com.score2;
+//강사님 풀이
+import com.score2.Score2_1;
+
+public class ScoreMain2_1 {
+
+	public static void main(String[] args) {
+
+		Score2_1 ob = new Score2_1();
+
+		ob.set();
+		ob.input();
+		ob.print();
+
+	}
+
+}
