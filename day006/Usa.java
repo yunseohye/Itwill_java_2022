b2fe9bc62ba46d82f7b95894bc05a21f772ef82a
yunseohye <97958499+yunseohye@users.noreply.github.com>
2022-06-10 16:22:38 +0900

    Create Usa.java

---
+++ day006/Usa.java
@@ -0,0 +1,23 @@
+package com.day006;
+
+import com.day007.Rect;
+
+public class Usa {
+
+	public static void main(String[] args) {
+
+		Rect ob = new Rect();
+		
+		ob.input();
+		int x = ob.area();
+		int y = ob.length();
+		ob.print(x, y);
+		
+		
+		
+		
+		
+		
+	}
+
+}
