367ae7705d0ffd83441ac764fe1ed0a484d67f11
yunseohye <97958499+yunseohye@users.noreply.github.com>
2022-06-14 19:34:15 +0900

    Create Test3.java

---
+++ day012/Test3.java
@@ -0,0 +1,21 @@
+package com.day012;
+
+import java.util.Calendar;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+
+		//오늘을 기준으로 1일 후 2022년 6월 15일 수요일
+		//년도? 2022
+		//월? 6
+		//일? 14
+		//몇일 뒤? 100
+		//결과값) 만난 날: 2022년 6월 14일 화요일
+		//결과값) 100일 후: 2022년 x월 x일 x요일 메소드 API에서 찾아서 풀기
+		
+		Calendar now = Calendar.getInstance();
+		
+	}
+
+}
