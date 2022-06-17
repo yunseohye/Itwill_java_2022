9a0931aaaf77aaadbfc6dbb0fa9ad49221e0d955
yunseohye <97958499+yunseohye@users.noreply.github.com>
2022-06-17 17:32:36 +0900

    Create Score.java

---
+++ score5/Score.java
@@ -0,0 +1,13 @@
+package com.score5;
+
+public interface Score {
+
+	public void input();
+	public boolean searchHak(String hak); //학번에 대한 검사를 해줌
+	public void print(); //출력
+	public void delete(); //학생
+	public void update(); //학생
+	public void findHak(); //학생
+	public void findName(); //학생
+	
+}
