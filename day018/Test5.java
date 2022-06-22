750a540a0bab3dbbbd2476005acd002915d0aa3f
yunseohye <97958499+yunseohye@users.noreply.github.com>
2022-06-22 19:51:52 +0900

    Create Test5.java

---
+++ day018/Test5.java
@@ -0,0 +1,37 @@
+package com.day018;
+
+import java.io.FileInputStream;
+
+/*
+파일 입출력
+FileInputStream
+FileOutourStrem
+*/
+
+//파일의 값을 불러와 읽어 내는 것
+public class Test5 {
+
+	public static void main(String[] args) {
+
+		try {
+		
+			FileInputStream fis = new FileInputStream("c:\\doc\\test.txt");
+			
+			int data;
+			
+			while((data=fis.read()) !=-1) {
+				
+				//System.out.print((char)data); 파일은 바이트로 읽어내면 값이 깨짐
+				System.out.write(data); //엔터가 없는 마지막줄은 출력이 되지 않음.
+				System.out.flush();//마지막에 엔터가 없는 줄 까지 출력해줘
+			}
+			
+			fis.close();
+			
+		} catch (Exception e) {
+
+		}
+		
+	}
+
+}
