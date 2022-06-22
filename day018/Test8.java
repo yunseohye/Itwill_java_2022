efcbc8d2d799b329be17570f524bb1844cd4d49c
yunseohye <97958499+yunseohye@users.noreply.github.com>
2022-06-22 19:52:01 +0900

    Create Test8.java

---
+++ day018/Test8.java
@@ -0,0 +1,65 @@
+package com.day018;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Scanner;
+
+public class Test8 {
+
+	public boolean filecopy(String str1, String str2) {
+
+		//이부분은 꼭 기억해두기!
+		File f = new File(str1);
+		
+		if(!f.exists()) {
+			return false;
+		}
+		//기억해두기~~~
+		
+		try {
+
+			FileInputStream fis = new FileInputStream(str1);
+			FileOutputStream fos = new FileOutputStream(str2);
+
+			int data;
+			byte[] buffer = new byte[1024];
+
+			while((data=fis.read(buffer, 0, 1024)) !=-1) {
+
+				fos.write(buffer, 0, data);
+
+			}
+
+			fos.close();
+			fis.close();
+
+		} catch (Exception e) {
+			System.out.println(e.toString());
+			return false;
+		}
+
+		return true;
+	}
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		Test8 ob = new Test8();
+		String str1, str2;
+
+		System.out.print("원본파일? ");
+		str1 = sc.next();
+		
+		System.out.print("대상파일? ");
+		str2 = sc.next();
+
+		if(ob.filecopy(str1, str2)) {
+			System.out.println("파일복사 성공!!");
+		} else {
+			System.out.println("파일복사 실패!!");
+		}
+
+	}
+
+}
