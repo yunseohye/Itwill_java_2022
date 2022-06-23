02178cf7c422a92b7c1486d131d9b382f0dc6f50
yunseohye <97958499+yunseohye@users.noreply.github.com>
2022-06-23 17:48:50 +0900

    Update Test5.java

--- day018/Test5.java
+++ day018/Test5.java
@@ -4,7 +4,7 @@ import java.io.FileInputStream;
 
 /*
 파일 입출력
-FileInputStream
+FileInputStream(사용빈도 높음)
 FileOutourStrem
 */
 
@@ -20,6 +20,7 @@ public class Test5 {
 			int data;
 			
 			while((data=fis.read()) !=-1) {
+				//반환값이 int인경우 무조건 아스키값으로 읽어와야함.
 				
 				//System.out.print((char)data); 파일은 바이트로 읽어내면 값이 깨짐
 				System.out.write(data); //엔터가 없는 마지막줄은 출력이 되지 않음.
