29d15099ddfffd4f25c2619cfa69ddd1a2293f6d
yunseohye <97958499+yunseohye@users.noreply.github.com>
2022-06-23 17:48:38 +0900

    Update Test12.java

--- day018/Test12.java
+++ day018/Test12.java
@@ -10,15 +10,17 @@ public class Test12 {
 		String str1 = "c:\\doc\\temp\\java\\test1.txt";
 
 		/*
-		방법1 temp폴더 만들기
+		방법1 temp폴더 만들기 (사용빈도 낮음)
 		String path = str1.substring(0,str1.lastIndexOf("\\"));
 		//System.out.println(path); 변수 값 검증
 
 		File f = new File(path);
 
+		실제로는 이 부분만 많이 사용한다.
 		if(!f.exists()) {
 			f.mkdirs();
 		}
+		mkdirs로 사용해야함. mkdir쓰지않게 유의할 것
 		 */
 
 		//방법2
