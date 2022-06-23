ac25c6e2f970e03fc588cf7e6087992a2d3158b4
yunseohye <97958499+yunseohye@users.noreply.github.com>
2022-06-23 17:49:05 +0900

    Create Test3.java

---
+++ day019/Test3.java
@@ -0,0 +1,38 @@
+package com.day019;
+
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class Test3 extends Frame{
+
+	public Test3() {
+		
+		setTitle("윈도우");
+		setSize(200,300);
+		
+		//종료버튼을 실행하는 코딩.
+		//Test2에서는 class를 객체로 생성하여 상속받은 WindowAdapter를 사용한것
+		//여기서는 하나로 합쳐서 WindowAdapter를 바로 생성한 것임.
+		addWindowListener(new WindowAdapter() {
+		
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		
+		});
+		
+		setVisible(true);
+		
+	}
+
+	
+	
+	public static void main(String[] args) {
+
+		new Test3();
+		
+	}
+
+}
